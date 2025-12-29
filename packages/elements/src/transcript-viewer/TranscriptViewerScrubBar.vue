c46fb38e422c18bd2af5e1252f6dbd6cbd5dc01d
Charlie Wang <18888351756@163.com>
2025-12-29 15:05:44 +0800

    feat: add transcript-viewer component (#24)
    * feat: add transcript-viewer component
    
    * chore: cleanup
    
    * chore: rename TranscriptViewer to TranscriptViewerContainer
    
    * chore: use emit events instead of callback props
    
    * chore: rename ClassName props to Class
    
    * docs: remove (Ref) annotations

---
+++ packages/elements/src/transcript-viewer/TranscriptViewerScrubBar.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@repo/shadcn-vue/lib/utils'
+import {
+  ScrubBarContainer,
+  ScrubBarProgress,
+  ScrubBarThumb,
+  ScrubBarTimeLabel,
+  ScrubBarTrack,
+} from '../scrub-bar'
+import { useTranscriptViewerContext } from './useTranscriptViewer'
+
+interface Props {
+  showTimeLabels?: boolean
+  labelsClass?: string
+  trackClass?: string
+  progressClass?: string
+  thumbClass?: string
+  class?: HTMLAttributes['class']
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showTimeLabels: true,
+})
+
+const {
+  duration,
+  currentTime,
+  seekToTime,
+  startScrubbing,
+  endScrubbing,
+} = useTranscriptViewerContext()
+
+function handleScrub(time: number) {
+  seekToTime(time)
+}
+
+function handleScrubStart() {
+  startScrubbing()
+}
+
+function handleScrubEnd() {
+  endScrubbing()
+}
+</script>
+
+<template>
+  <ScrubBarContainer
+    data-slot="transcript-scrub-bar"
+    :duration="duration"
+    :value="currentTime"
+    :class="props.class"
+    @scrub="handleScrub"
+    @scrub-start="handleScrubStart"
+    @scrub-end="handleScrubEnd"
+  >
+    <div class="flex flex-1 flex-col gap-1">
+      <ScrubBarTrack :class="props.trackClass">
+        <ScrubBarProgress :class="props.progressClass" />
+        <ScrubBarThumb :class="props.thumbClass" />
+      </ScrubBarTrack>
+      <div
+        v-if="showTimeLabels"
+        :class="cn(
+          'text-muted-foreground flex items-center justify-between text-xs',
+          props.labelsClass,
+        )"
+      >
+        <ScrubBarTimeLabel :time="currentTime" />
+        <ScrubBarTimeLabel :time="Math.max(0, duration - currentTime)" />
+      </div>
+    </div>
+  </ScrubBarContainer>
+</template>
---
+++ packages/elements/src/transcript-viewer/index.ts
@@ -0,0 +1,19 @@
+export { default as TranscriptViewerAudio } from './TranscriptViewerAudio.vue'
+export { default as TranscriptViewerContainer } from './TranscriptViewerContainer.vue'
+export { default as TranscriptViewerPlayPauseButton } from './TranscriptViewerPlayPauseButton.vue'
+export { default as TranscriptViewerScrubBar } from './TranscriptViewerScrubBar.vue'
+export { default as TranscriptViewerWord } from './TranscriptViewerWord.vue'
+export { default as TranscriptViewerWords } from './TranscriptViewerWords.vue'
+export { useTranscriptViewerContext } from './useTranscriptViewer'
+export { useTranscriptViewer } from './useTranscriptViewer'
+export type {
+  SegmentComposer,
+  TranscriptGap,
+  TranscriptSegment,
+  TranscriptViewerAudioProps,
+  TranscriptViewerAudioType,
+  TranscriptViewerContextValue,
+  TranscriptWord,
+  UseTranscriptViewerProps,
+  UseTranscriptViewerResult,
+} from './useTranscriptViewer'
